60b2504ba6e0731fa91a5b06ec76e6a656bd7e53
Denis Markov <dm.marik.230185@gmail.com>
2024-03-30 08:00:29 +0100

    prepLnk2Xsocket.sh : fixed bug caused by incorrect usage of [[ -e <symlink> ]]

--- prepLnk2Xsocket.sh
+++ prepLnk2Xsocket.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 # prepLnk2Xsocket.sh
+# Create / refresh symlink to X unix socket file of my current X session.
+# (To share it with Docker container through -v option.)
 
+# Location of the symlink:
 Lnk2unixSocket=${HOME}/.x-unix-socket
 
-# Obtain location of 'my' unix socket file
+# Obtain location of 'my' X unix socket file.
+# DISPLAY have the following format: <host>:<display>[.<screen>]
 displayNum=${DISPLAY##*:}
 socketNum=${displayNum%%.*}
 socketFile=/tmp/.X11-unix/X${socketNum}
 
-# [Re]Create symlink if it is required
-if [[ -e ${Lnk2unixSocket} ]] ; then
-  if [[ -L ${Lnk2unixSocket} ]] ; then
-    # Verify whether the link points to correct target
-    LnkTarget=$(readlink "${Lnk2unixSocket}")
-    if [[ "${LnkTarget}" = "${socketFile}" ]] ; then
-      exit 0
-    else # the symlink is NOT correct
-      # remove the symlink
-      rm -f "${Lnk2unixSocket}"
-    fi
-  else # file name conflict
-    echo "Filesystem object ${Lnk2unixSocket} exists and is NOT symlink."
-    echo "Can not create symlink ${Lnk2unixSocket} to ${socketFile}"
-    echo "File name conflict - solve it manually and run this script once again!"
-    exit 1
+# [Re]Create symlink if it is required.
+if [[ -L ${Lnk2unixSocket} ]] ; then
+  echo "Symlink exists!"
+  # Verify whether the link points to correct target
+  LnkTarget=$(readlink "${Lnk2unixSocket}")
+  echo "symlink: ${Lnk2unixSocket}"
+  echo "its target: ${LnkTarget}"
+  if [[ "${LnkTarget}" = "${socketFile}" ]] ; then
+    echo "The link is correct. Nothing to do."
+    exit 0
+  else 
+    # the symlink is NOT correct
+    echo "remove the symlink"
+    rm -f "${Lnk2unixSocket}"
   fi
 fi
+
+echo "Will create symlink ${Lnk2unixSocket} to ${Lnk2unixSocket}"
 ln -s "${socketFile}" "${Lnk2unixSocket}"

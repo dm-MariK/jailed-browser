50ee4353c7f8e97983d069b701a9e571740532c2
Denis Markov <dm.marik.230185@gmail.com>
2024-03-30 08:55:29 +0100

    bakeDockerXcookie.sh : extended comments added; dbg msgs commented out.

--- bakeDockerXcookie.sh
+++ bakeDockerXcookie.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 # bakeDockerXcookie.sh [trusted|untrusted]
+# Prepare XAUTHORITY MIT-MAGIC-COOKIE-1 for a Docker container by `xauth generate`.
+# Created cookies contain a server address family code (host identification) 
+# that must be overwritten as follows.
+# * bake a temporary trusted or untrusted cookie and put it to the temporary file,
+# * read the cookie from that file,
+# * change server address family code / host identification to "universal" one (ffff) 
+#   that means "any host",
+# * write the result to the final cookie file that will be shared with a Docker container,
+# * remove the temporary cookie file.
+# See here:
+# <https://github.com/mviereck/x11docker/wiki/X-authentication-with-cookies-and-xhost-(%22No-protocol-specified%22-error)>
+# <https://github.com/mviereck/x11docker/wiki/How-to-access-X-over-TCP-IP-network>
+
 # By default - no $1 is passed / unrecognized $1 - untrusted x-cookie is baked:
 cookieType="untrusted"
 # Generate a cookie that has to be used within 3600 seconds the first time:
@@ -8,7 +21,7 @@ Timeout=3600
 if [[ $1 && $1 = "trusted" ]] ; then
   cookieType="$1"
 fi
-echo ${cookieType} 
+echo "Will bake ${cookieType} cookie."
 
 # Define a file name for the x-cookie; remove existing x-cookie file.
 Cookiefile=${HOME}/.DockerXcookie.${cookieType}
@@ -16,24 +29,25 @@ if [[ -f ${Cookiefile} ]] ; then
   rm ${Cookiefile}
 fi
 
-
+# Create temporary cookie file.
 tmpCookiefile=${HOME}/tmp.Xcookie
 
 # xauth -f ~/mycookie generate $DISPLAY . untrusted timeout 3600
-xauth -q -f ${tmpCookiefile} generate ":0" . ${cookieType} timeout ${Timeout}
-echo "xuy-1"
-ls -la ${tmpCookiefile}
-xauth -i -f ${tmpCookiefile} list | awk '{print $3}'
+xauth -q -f ${tmpCookiefile} generate ":0" . ${cookieType} timeout ${Timeout} > /dev/null 2>&1
+#echo "boom-1"
+#ls -la ${tmpCookiefile}
+#xauth -i -f ${tmpCookiefile} list | awk '{print $3}'
 
-# replace host identification with ffff / family wild # <---------- CORRECT NAMING FOR THAT ???
+# Extract the the cookie string, replace host identification with ffff / family wild.
 Cookie="$(xauth -i -f ${tmpCookiefile} nlist | sed -e 's/^..../ffff/')"
 
-# ...
+# Write the adjusted cookie to the final cookie file, restrict its access rights.
 touch ${Cookiefile}
 echo "${Cookie}" | xauth -i -f ${Cookiefile} nmerge -
 chmod 600 ${Cookiefile}
 
+# Remove temporary cookie file.
 rm ${tmpCookiefile}
 
-xauth -i -f ${Cookiefile} list | awk '{print $3}'
-xauth -i -f ${Cookiefile} list 
+#xauth -i -f ${Cookiefile} list | awk '{print $3}'
+#xauth -i -f ${Cookiefile} list 
